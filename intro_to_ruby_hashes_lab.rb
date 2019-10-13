a0f9a0c283fac9fd3008eb2174d2c965e1a80b4c
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-13 04:29:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,6 @@
 def base_hash
-	{:hash => :key}
 	key = "railroads"
-	p hash
+	p :hash => :key
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 end
