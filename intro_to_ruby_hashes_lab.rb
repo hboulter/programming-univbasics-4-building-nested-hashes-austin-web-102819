3487e82739e4052e76b8183b592400c734564c9b
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-13 03:45:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
-def base_hash
-	p :key
+def base_hash {
+	p "hash" => {:key}
+}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
 end
 
 def monopoly_with_second_tier
